d685ea0130794ead290c88b171dbe21db71abd43
Lucas Navarro <navarrorlucas@gmail.com>
2023-08-04 20:52:52 +0200

    mvp done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+//imported data
 
+const Movies = require("../models/Movie.model.js");
+
+/* GET home page */
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movies.find()
+    .select({ title: 1, image: 1 })
+    .then((movie) => {
+      res.render("movies.hbs", {
+        movie,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+
+router.get("/movie/:movieid", (req, res, next) => {
+    
+    let movieId = req.params.movieid
+
+    Movies.findById(movieId)
+    .then((oneMovie)=> {
+        console.log(oneMovie)
+        res.render("movie-details.hbs", {
+            oneMovie
+            
+        })
+    })
+    .catch((error) => {
+        next(error);
+      });
+})
 module.exports = router;
